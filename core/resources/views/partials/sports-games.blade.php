f0e86a8def9e7622cf6a2b5a8cdcad0a2ba6a8a5
sumon <sumon626000@users.noreply.github.com>
2026-09-27 02:45:58 +0600

    Use local sports provider logos with padded contain tiles.

--- core/resources/views/partials/sports-games.blade.php
+++ core/resources/views/partials/sports-games.blade.php
@@ -5,55 +5,64 @@ $sportsGames = [
         'id' => '92b24e4c25107367a80e0fe1a97c24e4',
         'name' => 'Lucky Sport',
         'provider' => 'luckysport',
-        'img' => 'https://img.b6814jd.com/bjd/h5/assets/images/brand/white/provider-awcv2_fb.png?v=1&source=mcdsrc',
+        'img' => asset('assets/images/sports/lucky.webp'),
+        'tile' => 'dark',
     ],
     [
         'id' => '08ced9dd788aed11ff3c7f387ae0f063',
         'name' => 'SABA Sports',
         'provider' => 'sabasports',
-        'img' => 'https://img.b6814jd.com/bjd/h5/assets/images/brand/white/provider-saba.png?v=1&source=mcdsrc',
+        'img' => asset('assets/images/sports/saba.jpg'),
+        'tile' => 'light',
     ],
     [
         'id' => '4d31f1186a81e208c003a7e37411ce35',
         'name' => 'BTI Sports',
         'provider' => 'bti',
-        'img' => 'https://img.b6814jd.com/bjd/h5/assets/images/brand/white/provider-awcv2_bti.png?v=1&source=mcdsrc',
+        'img' => asset('assets/images/sports/bti.webp'),
+        'tile' => 'light',
     ],
     [
         'id' => '1f7fbf84bf1bcc08c3a7ea27db75f366',
         'name' => 'CMD Sports',
         'provider' => 'cmd',
-        'img' => 'https://img.b6814jd.com/bjd/h5/assets/images/brand/white/provider-awcv2_cmd.png?v=1&source=mcdsrc',
+        'img' => asset('assets/images/sports/cmd.png'),
+        'tile' => 'dark',
     ],
     [
         'id' => 'c4b2813f6bbc5abf502ddfb857e604eb',
         'name' => 'United Gaming',
         'provider' => 'ug',
-        'img' => 'https://img.b6814jd.com/bjd/h5/assets/images/brand/white/provider-awcv2_im.png?v=1&source=mcdsrc',
+        'img' => asset('assets/images/sports/ug.webp'),
+        'tile' => 'light',
     ],
     [
         'id' => '341827d4370bb198b18364e2d75e6916',
         'name' => 'SBO',
         'provider' => 'sbo',
-        'img' => 'https://img.b6814jd.com/bjd/h5/assets/images/brand/white/provider-saba.png?v=1&source=mcdsrc',
+        'img' => asset('assets/images/sports/sbo.png'),
+        'tile' => 'light',
     ],
     [
         'id' => '4ee8e0051a035b463b47c3c473ce317d',
         'name' => 'TF Sports',
         'provider' => 'tf',
-        'img' => '',
+        'img' => asset('assets/images/sports/tf.webp'),
+        'tile' => 'light',
     ],
     [
         'id' => '23c2dca76f87d7b7f239833060c8751e',
         'name' => 'DP Sports',
         'provider' => 'dpsports',
-        'img' => asset('assets/images/sports/dpsports.svg'),
+        'img' => asset('assets/images/sports/dpsports.png'),
+        'tile' => 'dark',
     ],
     [
         'id' => 'e130116fdc9bcde2dbb31735b6c365d6',
         'name' => 'DP Esports',
         'provider' => 'dpesports',
-        'img' => asset('assets/images/sports/dpesports.svg'),
+        'img' => asset('assets/images/sports/dpesports.jpg'),
+        'tile' => 'dark',
     ],
 ];
 
@@ -68,14 +77,15 @@ $fallbackSvg = function (string $label, string $bg1 = '#0f766e', string $bg2 = '
     @php
         $img = !empty($game['img']) ? $game['img'] : $fallbackSvg($game['name']);
         $prov = $game['provider'] ?? 'luckysport';
+        $tile = $game['tile'] ?? 'dark';
     @endphp
-    <div class="swiper-slide game-item-box game-card" data-category="sports" data-provider="{{ $prov }}" data-game-id="{{ $game['id'] }}" data-game-name="{{ $game['name'] }}" data-game-img="{{ $img }}">
+    <div class="swiper-slide game-item-box game-card sports-provider-card" data-category="sports" data-provider="{{ $prov }}" data-game-id="{{ $game['id'] }}" data-game-name="{{ $game['name'] }}" data-game-img="{{ $img }}">
         @auth
-            <a href="{{ url('user/jili/launch?game_code='.$game['id'].'&provider='.$prov) }}" class="game-card-img" title="{{ $game['name'] }}">
+            <a href="{{ url('user/jili/launch?game_code='.$game['id'].'&provider='.$prov) }}" class="game-card-img sports-logo-tile sports-logo-tile--{{ $tile }}" title="{{ $game['name'] }}">
         @else
-            <a href="{{ route('user.login') }}" class="game-card-img" title="{{ $game['name'] }}">
+            <a href="{{ route('user.login') }}" class="game-card-img sports-logo-tile sports-logo-tile--{{ $tile }}" title="{{ $game['name'] }}">
         @endauth
-                <img src="{{ $img }}" alt="{{ $game['name'] }}" loading="lazy" decoding="async" referrerpolicy="no-referrer"
+                <img src="{{ $img }}" alt="{{ $game['name'] }}" loading="lazy" decoding="async"
                      onerror="this.onerror=null;this.src='{{ $fallbackSvg($game['name']) }}';">
             </a>
         <div class="game-card-name">{{ $game['name'] }}</div>
